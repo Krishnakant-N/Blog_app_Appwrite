d39825b403768cfc8b1875b579990704729a7b0c
Krishnakant-N <krishnakant.13.niwaria@gmail.com>
2024-10-26 16:43:15 +0530

    new files and updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import "./App.css";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import {Outlet} from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}));
+        }else{
+          dispatch(logout());
+        }
+      })
+      .finally(setLoading(false));
+  }, []);
 
-  return (
-    <>
-      <h1>Blog app</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+          <Header />
+          <main>
+            TODO: <Outlet />
+          </main>
+          <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
